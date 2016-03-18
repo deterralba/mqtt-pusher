6373982d3651a12148a5a525294caae443d07d7d
Joseph Valleix <joseph.valleix@datadoghq.com>
2016-03-17 21:25:31 -0400

    fix subprocess issue

--- generate.py
+++ generate.py
@@ -6,6 +6,7 @@ import argparse
 import subprocess
 import time
 from random import randint
+import os
 
 # We parse the arguments
 #========================#
@@ -15,7 +16,7 @@ parser.add_argument('ip', help='IP where to send the messages')
 parser.add_argument('-p', '--port', type=int, default=1883, help='port where to send the messages')
 parser.add_argument('-t', '--topic',  default='topic', help='metric where to sent the messages')
 parser.add_argument('-s', '--sensor', default='temperature', help='type of sensor simulated')
-parser.add_argument('-w', '--wait', default=1, help='time in seconds between two messages')
+parser.add_argument('-w', '--wait', type=float, default=1, help='time in seconds between two messages')
 parser.add_argument('-v', '--verbose', default=False,
         action="store_true", help='verbose mode')
 
@@ -26,6 +27,9 @@ if args.verbose:
 
 # We create the payload
 #=======================#
+
+# if args.sensor not in sensors:
+#     raise ValueError('Invalid parameter sensor: "{}"'.format(sensor))
 
 def get_payload(sensor):
 
@@ -47,9 +51,19 @@ def get_payload(sensor):
 #==========================#
 
 while True:
-    proc = subprocess.Popen(['mosquitto_pub', '-h', str(args.ip), '-p', str(args.port), '-t', '"{}"'.format(args.topic),
-        '-m', '"{}"'.format(get_payload(args.sensor))], stdin=subprocess.PIPE)
-    proc.wait()
+    cmd = ['mosquitto_pub', '-h', str(args.ip), '-p', str(args.port), '-t', '"{}"'.format(args.topic),
+        '-m', '"{}"'.format(get_payload(args.sensor))]
+    if args.verbose:
+        cmd.append('-d')
+
+    if args.verbose:
+        print('Will send {}'.format(cmd))
+
+    # subprocess doesn't correctly send the cmd. Why? It's a mystery
+    # proc = subprocess.Popen(cmd)
+    # proc.wait()
+
+    os.popen(' '.join(cmd))
     time.sleep(args.wait)
 
 
